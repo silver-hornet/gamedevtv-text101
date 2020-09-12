926146baabf00e2d3c8302f1dea6ab6f7a41269f
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2020-09-12 10:25:59 -0400

    Initial commit

---
+++ Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    State state;
+
+    void Start()
+    {
+        state = startingState;
+        textComponent.text = state.GetStateStory();
+
+    }
+
+    void Update()
+    {
+        ManageState();
+    }
+
+    void ManageState()
+    {   
+        var nextStates = state.GetNextStates();
+        for (int index = 0; index < nextStates.Length; index++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + index))
+            {
+                state = nextStates[index];
+            }
+        }
+        textComponent.text = state.GetStateStory();
+    }
+}
